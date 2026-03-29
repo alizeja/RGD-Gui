f5a2a8109f6ecc5d47e08390091f1471dd0716ff
alizeja <piggyxd123xd@gmail.com>
2026-03-29 12:01:18 +0000

    cool

--- rgd.lua
+++ rgd.lua
@@ -388,7 +388,7 @@ local runLoop = RunService.Heartbeat:Connect(function()
                 humanoid.Died:Connect(function()
                     -- queue_on_teleport([[
                         _G.autoFarm = true
-                        
+                        loadstring(game:HttpGet("https://raw.githubusercontent.com/alizeja/RGD-Gui/refs/heads/main/rgd.lua"))()
                     -- ]])
                     GE:FireServer("Restart")
                 end)
